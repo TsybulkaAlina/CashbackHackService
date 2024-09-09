985692aed84e7a6ea93b0b1e8e3ce7081673bc6d
Alina Tibulca <tibulkalina@mail.ru>
2024-09-09 14:10:54 +0200

    add tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,2 +1,17 @@
-package ru.netology.service;public class CashbackHackServiceTest {
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(service.remain(900), 100);
+        Assert.assertEquals(service.remain(1000), 0);
+        Assert.assertEquals(service.remain(1100), 900);
+    }
 }
+
+
